9b835e163140217ac869ef6d4334cd1f5a3b4ba9
shobhit0101 <shobhitgpt01@gmail.com>
2022-03-30 03:51:07 +0530

    done

--- app.js
+++ app.js
@@ -1,16 +1,42 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
+const User = require('./models/User');
 //express app
 const app = express();
+
+//connect to mongodb
+const dbURI='mongodb+srv://Vital:Vital@realestate.bhuwn.mongodb.net/real_database?retryWrites=true&w=majority';
+mongoose.connect(dbURI , { useNewUrlParser: true, useUnifiedTopology: true })
+.then((result) =>  app.listen(3000 ))
+.catch((err)=>console.log(err));
+
+
 
 //register view engine
 app.set('view engine', 'ejs');
 app.set('views', 'ejs_files');
 
-
-//listen for request
-app.listen(3000);
+//middleware & static files
 app.use(express.static('public'));
+
+//mongoose and mongo sandbox routes
+app.get('/add-User',( req, res)=>{
+   const user = new User({
+      username: 'Vital',
+      password: 'Vital',
+      email: 'shobhitgpt01@gmail.com'
+   });
+   user.save()
+   .then(result=>{
+      res.send(result)}) 
+      .catch(err=>{
+          console.log(err);
+      });
+} )
+
+
+//routes
+
 app.get('/', (req,res) => {
    res.render('home',{title : 'Home'});
 });
@@ -31,6 +57,10 @@ app.get('/postyourproperty', (req,res) => {
  app.get('/login', (req,res) => {
     res.render('login',{title : 'Login'});
  });
+
+ app.get('/viewproperty', (req,res) => {
+   res.render('viewproperty',{title : 'View Property'});
+});
 
  app.use((req ,res) => {
     res.status(404).render('404',{title : '404'});
--- public/js/LogIn_SignUp.js
+++ public/js/LogIn_SignUp.js
@@ -1,11 +1,91 @@
-const sign_in_btn = document.querySelector("#sign-in-btn");
-const sign_up_btn = document.querySelector("#sign-up-btn");
-const container = document.querySelector(".container");
+// const sign_in_btn = document.querySelector("#sign-in-btn");
+// const sign_up_btn = document.querySelector("#sign-up-btn");
+// const container = document.querySelector(".container");
 
-sign_up_btn.addEventListener("click", () => {
-  container.classList.add("sign-up-mode");
-});
+// sign_up_btn.addEventListener("click", () => {
+//   container.classList.add("sign-up-mode");
+// });
 
-sign_in_btn.addEventListener("click", () => {
-  container.classList.remove("sign-up-mode");
+// sign_in_btn.addEventListener("click", () => {
+//   container.classList.remove("sign-up-mode");
+// });
+
+
+function setFormMessage(formElement, type, message) {
+  const messageElement = formElement.querySelector(".form__message");
+
+  messageElement.textContent = message;
+  messageElement.classList.remove("form_message--success", "form_message--error");
+  messageElement.classList.add(`form__message--${type}`);
+}
+
+function setInputError(inputElement, message) {
+  inputElement.classList.add("form__input--error");
+  inputElement.parentElement.querySelector(".form__input-error-message").textContent = message;
+}
+
+function clearInputError(inputElement) {
+  inputElement.classList.remove("form__input--error");
+  inputElement.parentElement.querySelector(".form__input-error-message").textContent = "";
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const loginForm = document.querySelector("#login");
+  const createAccountForm = document.querySelector("#createAccount");
+
+  document.querySelector("#linkCreateAccount").addEventListener("click", e => {
+      e.preventDefault();
+      loginForm.classList.add("form--hidden");
+      createAccountForm.classList.remove("form--hidden");
+  });
+
+  document.querySelector("#linkLogin").addEventListener("click", e => {
+      e.preventDefault();
+      loginForm.classList.remove("form--hidden");
+      createAccountForm.classList.add("form--hidden");
+  });
+
+  loginForm.addEventListener("submit", e => {
+      e.preventDefault();
+
+      // Perform your AJAX/Fetch login
+
+      setFormMessage(loginForm, "error", "Invalid username/password combination");
+  });
+
+  document.querySelectorAll(".form__input").forEach(inputElement => {
+      inputElement.addEventListener("blur", e => {
+          if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 10) {
+              setInputError(inputElement, "Username must be at least 10 characters in length");
+          }
+          if (e.target.id === "email" && e.target.value.length > 0 && e.target.value.length < 10) {
+              setInputError(inputElement, "email must be at least 10 characters in length");
+           }
+          
+          if (e.target.id === "email") {
+              
+              if(!(e.target.value.includes('@')))
+                  setInputError(inputElement, "Format of email is wrong: must contain @");
+                  
+                      
+              
+          }
+          let a;
+          if (e.target.id === "pass" && e.target.value.length > 0 && e.target.value.length < 8) {
+              a=e.target.value;
+              setInputError(inputElement, "password must be at least 8 characters in length");
+           }
+           if (e.target.id === "pass2" && e.target.value!=a) {
+              setInputError(inputElement, "passwords don't match");
+           }
+           let phoneno = /^\d{10}$/;
+           if (e.target.id === "mob" && !e.target.value.match(phoneno)) {
+              setInputError(inputElement, "invalid mobile no.");
+           }
+      });
+
+      inputElement.addEventListener("input", e => {
+          clearInputError(inputElement);
+      });
+  });
 });
