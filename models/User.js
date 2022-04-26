46cd6f38b128320612ecb8d920c2503c7c97e510
lb0809 <lakshya.b20@iiits.in>
2022-04-26 23:39:06 +0530

    lb

--- models/User.js
+++ models/User.js
@@ -15,6 +15,10 @@ const UserSchema = new Schema({
         type: String,
         required: true
     },
+    phoneno: {
+        type: String,
+        required: true
+    },
    
 }, {timestamps: true});
 
